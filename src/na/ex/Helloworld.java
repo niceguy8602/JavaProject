20e260c69fc969ede809b707d3a902b60f477e8a
niceguy8602 <niceguy8602@DESKTOP-VEN6OHP>
2021-07-16 04:06:15 +0900

    eclipse branch commit;

--- src/na/ex/Helloworld.java
+++ src/na/ex/Helloworld.java
@@ -6,7 +6,10 @@ public class Helloworld {
 		System.out.println("hello niceguy.");
 		System.out.println("hello jihea.");
 		System.out.println("he");
+		System.out.println("new eclipse branch");
 
 	}
 
 }
+
+
