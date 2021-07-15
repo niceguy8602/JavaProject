dc4de06fa1ebe0c452868327b97b1cda6712d03f
niceguy8602 <niceguy8602@DESKTOP-VEN6OHP>
2021-07-16 03:15:18 +0900

    추가

--- src/na/ex/Helloworld.java
+++ src/na/ex/Helloworld.java
@@ -5,6 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello niceguy.");
 		System.out.println("hello jihea.");
+		System.out.println("he");
 
 	}
 
